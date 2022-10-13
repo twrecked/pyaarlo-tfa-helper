52143385828ecebe4cf5c39cfb745762f6350c41
Steve Herrell <steve.herrell@gmail.com>
2022-10-13 16:19:59 -0400

    Tidied some more stuff.

---
+++ google-app-engine/google-auth/static/copy-to-clipboard.js
@@ -0,0 +1,35 @@
+/*
+  Borrowed from here:
+  https://www.roboleary.net/2022/01/13/copy-code-to-clipboard-blog.html
+  Many thanks!
+ */
+
+const copyButtonLabel = "Copy Configuration";
+
+// use a class selector if available
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+  // only add a button if browser supports Clipboard API
+  if (navigator.clipboard) {
+    let button = document.createElement("button");
+    button.innerText = copyButtonLabel;
+    button.addEventListener("click", copyCode);
+    block.appendChild(button);
+  }
+});
+
+async function copyCode(event) {
+  const button = event.srcElement;
+  const pre = button.parentElement;
+  let code = pre.querySelector("code");
+  let text = code.innerText;
+  await navigator.clipboard.writeText(text);
+
+  button.innerText = "Configuration Copied";
+
+  setTimeout(()=> {
+    button.innerText = copyButtonLabel;
+  },1000)
+}
+
